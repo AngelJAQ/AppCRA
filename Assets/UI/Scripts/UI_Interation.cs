64e2f2551dd7e153353c36eec6f3cccd52e5c2a0
Angel Jesus Atenco Quiroz <AngelAtenco.AJAQ09@gmail.com>
2026-06-26 10:15:49 -0600

    IU
    Diseño IU

---
+++ Assets/UI/Scripts/UI_Interation.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Interation{
+    //Evento para el boton y acceder al inicio de capacitacion    
+    public void iniciar_Capacitacion(){
+
+    }
+
+    //Evento para el boton y acceder a los diferentes modulos de capacitacion
+    public void modulos_Capacitacion(){
+
+    }
+}
